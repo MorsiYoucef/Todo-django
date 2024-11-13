610ac58366d09a1852120f1ed3f1b3d2f410b586
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-11-13 15:14:46 +0100

    new commit

--- todoprj/todoapp/views.py
+++ todoprj/todoapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -13,9 +14,33 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
+        if (len(password) < 8 ):
+            messages.error(request, 'Password must be at least 8 characters')
+            return redirect('register')
+        
+        get_all_users_by_username = User.objects.filter(username=username)
+
+        if get_all_users_by_username:
+            messages.error(request, 'Error, username already exists, Use another')
+            return redirect('register')
+
+
+
         new_user = User.objects.create_user(username=username, email=email, password=password)
         new_user.save()
     return render(request, 'todoapp/register.html',{})
 
 def loginpage(request):
+    if request.method == 'POST':
+        username = request.POST.get('uname')
+        password = request.POST.get('pass')
+
+        validate_user = authenticate(username=username, password=password)
+        if validate_user is not None:
+            login(request, validate_user)
+            return redirect('home-page')
+        else:
+            messages.error(request, 'Error, wrong user details or user does not exist')
+            return redirect('login')
+
     return render(request, 'todoapp/login.html',{})
